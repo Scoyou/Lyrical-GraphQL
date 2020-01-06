d517e1a21d462467251e62f95fb4f5de11011ffc
Scott Young <youngscottj@gmail.com>
2020-01-06 15:20:49 -0700

    build out SongCreate component

--- client/index.js
+++ client/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
-import SongList from './components/SongList';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 import App from './components/App'
+import SongCreate from './components/SongCreate'
+import SongList from './components/SongList';
 
 const client = new ApolloClient({});
 
@@ -15,6 +17,7 @@ const Root = () => {
         <Route path='/' component={App}>
           <IndexRoute component={SongList} />
         </Route>
+        <Route path="song/new" component={SongCreate} />
       </Router>
     </ApolloProvider>
   )
