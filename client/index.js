de790fbc893f9155591f99a7b7f23770f01fe48c
Scott Young <youngscottj@gmail.com>
2020-01-07 03:21:04 -0700

    styling

--- client/index.js
+++ client/index.js
@@ -1,8 +1,10 @@
+import './style/style.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+
 
 import App from './components/App'
 import SongCreate from './components/SongCreate'
